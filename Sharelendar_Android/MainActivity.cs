dab5e26a8b1c20bfd5d04561fec2cd415f1ca3c0
Max Steiger <max.steiger@iet-gibb.ch>
2016-11-08 10:04:20 +0100

    Projektdateien hinzufügen.

---
+++ Sharelendar_Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Sharelendar_Android
+{
+    [Activity(Label = "Sharelendar_Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
